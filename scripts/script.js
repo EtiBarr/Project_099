bfde96914b9b764dda59b3b97e5c1da599d64506
EtiBarr <etienne.barriere.1@ens.etsmtl.ca>
2024-02-02 18:22:13 -0500

    sidebar and reactive content clean up

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Get references to the button and aside
+
     var toggleButton = document.getElementById('toggleButton');
     var aside = document.querySelector('aside');
+    var content = document.querySelector('content');
 
-    // Add a click event listener to the button
     toggleButton.addEventListener('click', function () {
-        // Toggle the 'collapsed' class on the aside element
+
         aside.classList.toggle('collapsed');
+        content.classList.toggle('collapsed');
+
     });
 });
