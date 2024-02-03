4c1f6ec2e82837ac0280242c8573f1c1eae1ab84
EtiBarr <etienne.barriere.1@ens.etsmtl.ca>
2024-02-02 21:08:23 -0500

    background and styling

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-
     var toggleButton = document.getElementById('toggleButton');
     var aside = document.querySelector('aside');
     var content = document.querySelector('content');
 
     toggleButton.addEventListener('click', function () {
-
-        aside.classList.toggle('collapsed');
+        var isCollapsed = aside.classList.toggle('collapsed');
         content.classList.toggle('collapsed');
 
+        toggleButton.innerText = isCollapsed ? '➕' : '➖';
     });
 });
